ed3f3155ddc2b99e3e61a57441f4d0071d946cef
poyigi <adepoyigiemmanuel@yahoo.com>
2022-10-24 02:55:44 -0400

    fizzbuzz

---
+++ src/com/poyigi/Main.java
@@ -0,0 +1,17 @@
+package com.poyigi;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println(i + " FizzBuzz");
+            } else if (i % 2 == 0) {
+                System.out.println(i + " Fizz");
+            } else if (i % 3 == 0) {
+                System.out.println(i + " Buzz");
+            } else {
+                System.out.println(i + " kolewerk");
+            }
+        }
+    }
+    }
